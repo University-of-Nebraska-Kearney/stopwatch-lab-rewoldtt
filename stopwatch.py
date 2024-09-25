8a0fc4cdfb5fc8f887552df25a2ad7bd3cd7076a
rewoldtt <ewoldtr@lopers.unk.edu>
2024-09-25 16:18:03 +0000

    Stopwatch

--- stopwatch.py
+++ stopwatch.py
@@ -1,5 +1,7 @@
 # Import Library
 import datetime
+SECONDS_PER_HOUR = 3600
+SECONDS_PER_MINUTE = 60
 
 # Get time for start
 input("Press Enter to start.")
@@ -14,3 +16,29 @@ stop_time = str(datetime.datetime.now().time()).split(":")
 stop_hour = stop_time[0]
 stop_minute = stop_time[1]
 stop_second = stop_time[2]
+
+# Convert hours to seconds
+act_stop_hour = int(stop_hour) * SECONDS_PER_HOUR
+act_start_hour = int(start_hour) * SECONDS_PER_HOUR
+#Convert minutes to seconds
+act_stop_minute = int(stop_minute) * SECONDS_PER_MINUTE
+act_start_minute = int(start_minute) * SECONDS_PER_MINUTE
+
+# Calculate total seconds
+start_time_in_seconds = int(act_start_hour) + int(act_start_minute) + float(start_second)
+stop_time_in_seconds = int(act_stop_hour) + int(act_stop_minute) + float(stop_second)
+total_seconds = float(stop_time_in_seconds) - float(start_time_in_seconds)
+
+# Turn seconds back to times
+end_hour = int(total_seconds) // SECONDS_PER_HOUR
+end_hour_in_minutes = end_hour * SECONDS_PER_MINUTE
+end_hour_in_seconds = end_hour_in_minutes * SECONDS_PER_MINUTE
+end_minute = (int(total_seconds) // SECONDS_PER_MINUTE) - int(end_hour_in_minutes)
+end_minute_in_seconds = end_minute * SECONDS_PER_MINUTE
+end_second = (float(total_seconds) - float(end_hour_in_seconds) - float(end_minute_in_seconds))
+
+# Get time for end
+end_time = (f"{end_hour}:{end_minute}:{end_second:.2f}")
+
+#show end result
+print(f"{end_time}")
